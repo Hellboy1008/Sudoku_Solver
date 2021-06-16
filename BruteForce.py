557fcf2704eeacee4155e663825cba18edeef200
Stephen Lee <srlee@ucsd.edu>
2021-06-15 18:21:05 -0700

    remove brute force file and change formatting of test files

--- BruteForce.py
+++
@@ -1,9 +0,0 @@
-# Created by: 龍ONE
-# Date Created: October 14, 2020
-# Date Edited: October 23, 2020
-# Purpose: Brute forces an incomplete sudoku puzzle.
-
-
-def brute_force(sudoku):
-    # brute force a sudoku puzzle
-    return 0
