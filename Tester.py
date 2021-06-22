b9d297fe3ae4a76b8a19bdb3f3bca75f68db98ab
Stephen Lee <srlee@ucsd.edu>
2021-06-22 15:44:02 -0700

    create tester file

---
+++ Tester.py
@@ -0,0 +1,7 @@
+# Created by: 龍ONE
+# Date Created: June 15, 2021
+# Date Edited: June 15, 2021
+# Purpose: To test the sudoku solver on puzzles of different difficulty levels.
+
+import SudokuSolver as solver
+
